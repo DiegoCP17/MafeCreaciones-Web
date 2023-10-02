82113e384f7e47c55503039e9ad6e3ac88f4eae7
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-10-02 17:58:37 -0500

    carrusel de img y estilos nuevos

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
 import MafeBanner from "../../assets/Banner/MafeBanner.png";
-import { BannerContainer, BannerImage } from "./StyleBanner";
+import MafeBanner2 from "../../assets/Banner/MafeBanner2.png";
+import MafeBanner3 from "../../assets/Banner/MafeBanner3.png";
+
+import {
+  BannerContainer,
+  BannerImage,
+  StyledArrowBackIosIcon,
+  StyledArrowForwardIosIcon,
+} from "./StyleBanner";
 
 export const Banner = () => {
+  const images = [MafeBanner, MafeBanner2, MafeBanner3];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const gotoPrevious = () => {
+    const isFirtSlide = currentIndex === 0;
+    const newIndex = isFirtSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const gotoNext = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <BannerContainer disableGutters>
-      <BannerImage 
-        src={MafeBanner}
-        alt="MafeBanner"
+      <BannerImage
+        src={images[currentIndex]}
+        alt="MafeBanners"
       />
+      <StyledArrowBackIosIcon onClick={gotoPrevious} />
+      <StyledArrowForwardIosIcon onClick={gotoNext} />
     </BannerContainer>
   );
 };
--- src/components/Banner/StyleBanner.js
+++ src/components/Banner/StyleBanner.js
@@ -1,5 +1,7 @@
 import { Container } from "@mui/system";
 import styled from "styled-components";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export const BannerContainer = styled(Container)`
   margin-bottom: 20px;
@@ -9,4 +11,36 @@ export const BannerImage = styled.img`
   width: 100%;
   height: auto;
   border-radius: 10px;
+`;
+
+export const StyledArrowForwardIosIcon = styled(ArrowForwardIosIcon)`
+  && {
+    position: absolute;
+    top: 45%;
+    transform: translate(0, -50%);
+    right: 160px;
+    font-size: 45px;
+    color: #000;
+    cursor: pointer;
+
+    @media (max-width: 768px) {
+      font-size: 25px;
+    }
+  }
+`;
+
+export const StyledArrowBackIosIcon = styled(ArrowBackIosIcon)`
+  && {
+    position: absolute;
+    top: 45%;
+    transform: translate(0, -50%);
+    left: 160px;
+    font-size: 45px;
+    color: #000;
+    cursor: pointer;
+
+    @media (max-width: 768px) {
+      font-size: 25px;
+    }
+  }
 `;
