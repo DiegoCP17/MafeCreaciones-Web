73fbe5aba8629a4dcc3fa6ffcd2333a9d83b71ce
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-10-02 17:27:31 -0500

    Creacion de banner y estilos

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import MafeBanner from "../../assets/Banner/MafeBanner.png";
+import { BannerContainer, BannerImage } from "./StyleBanner";
 
 export const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <BannerContainer disableGutters>
+      <BannerImage 
+        src={MafeBanner}
+        alt="MafeBanner"
+      />
+    </BannerContainer>
+  );
+};
---
+++ src/components/Banner/StyleBanner.js
@@ -0,0 +1,12 @@
+import { Container } from "@mui/system";
+import styled from "styled-components";
+
+export const BannerContainer = styled(Container)`
+  margin-bottom: 20px;
+`;
+
+export const BannerImage = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+`;
