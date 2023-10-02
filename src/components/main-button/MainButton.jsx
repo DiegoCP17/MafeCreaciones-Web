2cb674ada1d1c422dd13da87216f292fbece419b
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-10-02 15:40:46 -0500

    Repositorio limpio

--- src/components/main-button/MainButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Button } from "@mui/material";
-
-const MainButton = ({ iconImg, text }) => {
-  return <></>;
-};
-
-export default MainButton;
--- src/components/search-nav-item/SearchNavItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Box, Typography } from "@mui/material";
-
-const SearchNavItem = ({ normalText, boldText, iconImg }) => {
-  return <></>;
-};
-
-export default SearchNavItem;
