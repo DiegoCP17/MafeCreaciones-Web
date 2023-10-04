3b95b2794a1e0756452e11e656156eb8e6015866
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-10-04 14:52:09 -0500

    nuevo componente para renderizar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Appbar } from "../components/appbar/Appbar";
 import { Banner } from "../components/Banner/Banner";
+import { Gallery } from "../components/Gallery/Gallery";
 
 export const Home = () => {
   return (
     <div>
       <Appbar />
       <Banner />
+      <Gallery/>
     </div>
   );
 };
