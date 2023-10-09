fffae3bbd5fc2f99d9ba9161c63d2939b78a412a
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-10-09 15:26:59 -0500

    se agrega el componente AboutUs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Appbar } from "../components/appbar/Appbar";
 import { Banner } from "../components/Banner/Banner";
 import { Gallery } from "../components/Gallery/Gallery";
+import { AboutUS } from "../components/AboutUs/AboutUS";
 
 export const Home = () => {
   return (
     <div>
       <Appbar />
       <Banner />
-      <Gallery/>
+      <Gallery />
+      <AboutUS />
     </div>
   );
 };
