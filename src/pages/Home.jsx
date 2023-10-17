b67d66fd8c5aca30e54e3786fad28367fecc2daa
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-10-17 15:57:04 -0500

    Se agrega componente element para redireccionar los componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,24 @@ import { Gallery } from "../components/Gallery/Gallery";
 import { AboutUS } from "../components/AboutUs/AboutUS";
 import { Contact } from "../components/Contact/Contact";
 
+import { Element } from "react-scroll";
+
 export const Home = () => {
   return (
     <div>
       <Appbar />
-      <Banner />
-      <Gallery />
-      <AboutUS />
-      <Contact />
+      <Element name="inicio">
+        <Banner />
+      </Element>
+      <Element name="creaciones">
+        <Gallery />
+      </Element>
+      <Element name="acerca de">
+        <AboutUS />
+      </Element>
+      <Element name="contacto">
+        <Contact />
+      </Element>
     </div>
   );
 };
